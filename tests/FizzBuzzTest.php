d1745abd82f7c76a986a0c2729ac99c1f870cf3b
vandamme <arnaud.vandamme@circularx.co>
2023-04-07 12:00:35 +0200

    add a new test print_2

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -8,4 +8,16 @@ final class FizzBuzzTest extends TestCase
    {
       $this->assertEquals(FizzBuzz::print(1), '1');
    }
+   public function test_print_2(): void 
+   {
+      $this->assertEquals(FizzBuzz::print(2), '2');   
+   }
+}
+
+final class FizzBuzz {
+   public static function print(int $number):string
+   {
+      return '1';
+   }
+   
 }
