0dfd07911b3536fc2642464c618754e72344d893
Gildas Quéméner <gildas.quemener@gmail.com>
2023-04-07 11:45:23 +0200

    Express first failing test

---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class FizzBuzzTest extends TestCase
+{
+   public function test_print_1(): void
+   {
+      $this->assertEquals(FizzBuzz::print(1), '1');
+   }
+}
